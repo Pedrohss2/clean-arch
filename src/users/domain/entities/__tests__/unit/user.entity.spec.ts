7d491fd84c3d4e2395f2840989194eb308e4cebe
Pedrohss2 <pedro2266809ph@gmail.com>
2026-03-12 22:50:59 -0300

    feat: Add class-validator and update user entity with setters and validation logic

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -23,6 +23,13 @@ describe("UserEntity unit tests", () => {
     expect(typeof sut.name).toBe("string");
   })
 
+  it("Setter of name field", () => {
+    sut['name'] = "new name test";
+
+    expect(sut.props.name).toEqual("new name test");
+    expect(typeof sut.name).toBe("string");
+  })
+
   it("Getter of email field", () => {
     expect(sut.email).toBeDefined();
     expect(sut.email).toEqual(props.email);
@@ -35,11 +42,29 @@ describe("UserEntity unit tests", () => {
     expect(typeof sut.password).toBe("string");
   })
 
+  it("Setter of password field", () => {
+    sut['password'] = "new password test";
+
+    expect(sut.props.password).toEqual("new password test");
+    expect(typeof sut.password).toBe("string");
+  })
+
   it("Getter of createdAt field", () => {
     expect(sut.createdAt).toBeDefined();
     expect(sut.createdAt).toBeInstanceOf(Date);
   })
 
+  it("Setter update a user", () => {
+    sut.update("new name test");
+
+    expect(sut.props.name).toEqual("new name test");
+  })
+
+  it("Should update the password field", () => {
+    sut.updatePassword("new password test");
+
+    expect(sut.props.password).toEqual("new password test");
+  })
 })
 
 
